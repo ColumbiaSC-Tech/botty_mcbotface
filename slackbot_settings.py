41f293b5f6ee3d395424fc576be7ecb02cdcc284
Danny <danny@nulleffort.com>
2017-09-10 14:29:25 -0400

    removed env files

--- slackbot_settings.py
+++
@@ -1,7 +0,0 @@
-API_TOKEN = 'xoxb-236517233687-8zeO6smry8jRL7AK2xxQjGU3'
-DEFAULT_REPLY = "Sorry but I didn't understand you"
-ERRORS_TO = 'danny'
-PLUGINS = [
-    'slackbot.plugins',
-    'botty_mcbotface',
-]
