b8b928e0af255fe3b3b2e190b5e48b6ecb3d224d
Danny <danny@nulleffort.com>
2017-09-10 13:19:27 -0400

    removed testing dir

--- botty_testing/run.py
+++
@@ -1,9 +0,0 @@
-from slackbot.bot import Bot
-
-
-def main():
-    bot = Bot()
-    bot.run()
-
-if __name__ == "__main__":
-    main()
--- botty_testing/slackbot_settings.py
+++
@@ -1,3 +0,0 @@
-API_TOKEN = 'xoxb-236517233687-8zeO6smry8jRL7AK2xxQjGU3'
-DEFAULT_REPLY = "Sorry but I didn't understand you"
-ERRORS_TO = 'danny'
