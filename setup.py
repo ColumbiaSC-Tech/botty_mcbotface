a99a1dc4615408d0dba7e068a2c69375408a6052
Danny <danny@nulleffort.com>
2017-09-10 13:46:06 -0400

    general setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='botty_mcbotface',
+      version='0.1',
+      description='General purpose Slack-bot with personality',
+      url='tbd',
+      author='Danny Hinshaw',
+      author_email='danny@nulleffort.com',
+      # install_requires=[],
+      license='MIT',
+      packages=['botty_mcbotface'],
+      zip_safe=False)
