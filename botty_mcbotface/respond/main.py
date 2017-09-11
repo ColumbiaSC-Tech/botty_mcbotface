e93b1096d18681c3a960f66f8273dacc74cff7dc
Danny <danny@nulleffort.com>
2017-09-10 20:00:41 -0400

    started help response

--- botty_mcbotface/respond/main.py
+++ botty_mcbotface/respond/main.py
@@ -6,16 +6,19 @@ from slackbot.bot import listen_to, respond_to, re
 
 @respond_to('^(arise|wake)', re.IGNORECASE)
 def arise(message, command):
+    # message.react('+1')
     responses = [
         '_Up from the 36 chamber!!!!_',
         ':fire::fire::fire:ᕦ໒( ᴼ 益 ᴼ )७ᕤ:fire::fire::fire:'
     ]
     return message.send(responses[1])
-    # react with thumb up emoji
-    # message.react('+1')
 
 
-@respond_to('I love you', re.IGNORECASE)
+@respond_to('^help', re.IGNORECASE)
 def love(message):
-    message.reply('I love you too!')
+    help_msg = '\nI\'m not a very smart baht...\n' \
+               'You can command me with _dot_ commands.\n' \
+               'Here\'s what I know so far\n' \
+               '`.g <search term>` - Search google and return the first result'
+    return message.reply(help_msg)
 
