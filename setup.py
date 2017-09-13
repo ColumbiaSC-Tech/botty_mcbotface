de78668777d31a959ce6372db10f08d259e273f5
Danny <danny@nulleffort.com>
2017-09-13 16:52:29 -0400

    added readme.rst for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
-
+# For converting markdown README to rst: pandoc --from=markdown --to=rst README.md -o README.rst
 setup(name='botty_mcbotface',
       version='0.1',
       description='General purpose Slack-bot with personality',
-      url='tbd',
+      url='https://github.com/ColumbiaSC-Tech/botty_mcbotface',
       author='Danny Hinshaw',
       author_email='danny@nulleffort.com',
       install_requires=[
