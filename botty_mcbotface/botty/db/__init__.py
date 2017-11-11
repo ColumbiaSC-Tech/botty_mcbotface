b5dead0be47e11eb447c62d15b70617d0d187208
Danny <danny@nulleffort.com>
2017-11-11 13:13:50 -0500

    made db tasks threaded

--- botty_mcbotface/botty/db/__init__.py
+++ botty_mcbotface/botty/db/__init__.py
@@ -1,12 +1,12 @@
 import asyncio
 import concurrent.futures
+import threading
 import os
 from botty_mcbotface.botty.api import get_all_channels, get_all_users
 from sqlalchemy import create_engine, insert
 from sqlalchemy.orm import Query, scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import MetaData
-from time import sleep
 
 
 class BottyDB:
@@ -45,6 +45,14 @@ Base.metadata.create_all(bind=db.engine)
 
 def db_add_row(row):
     sess = db.session()
+    sess.add(row)
+    sess.commit()
+    sess.close()
+    # sess.execute(insert(row, prefixes=['OR IGNORE']))
+
+
+def db_merge_row(row):
+    sess = db.session()
     sess.merge(row)
     sess.commit()
     sess.close()
@@ -52,14 +60,16 @@ def db_add_row(row):
 
 
 def populate_channels():
+
     chans = get_all_channels().body['channels']
 
     for c in chans:
         if not c['is_private']:
-            db_add_row(Channel(id=c['id'], name=c['name']))
+            db_merge_row(Channel(id=c['id'], name=c['name']))
 
 
 def populate_users():
+
     users = get_all_users().body['members']
 
     for u in users:
@@ -67,35 +77,44 @@ def populate_users():
             s_id = u['id']
             s_name = u['name']
             _id = s_name + s_id
-            db_add_row(User(id=_id, slack_id=s_id, slack_name=s_name))
-
-
-# Start an event loop for async requests
-loop = asyncio.get_event_loop()
+            db_merge_row(User(id=_id, slack_id=s_id, slack_name=s_name))
 
 
 @asyncio.coroutine
-def populate_periodic():
-    # while True:
+def populate_periodic(_loop):
+
     print('populate_periodic::RUNNING')
 
     tasks = [populate_channels, populate_users]
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=len(tasks))
+    # print(executor.__dict__)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        futures = [loop.run_in_executor(executor, t) for t in tasks]
-        yield from asyncio.gather(*futures)
-        print('populate_periodic::SLEEP')
-        yield from asyncio.sleep(15)
-        yield from populate_periodic()
+    def periodic(loop):
+        with executor:
+            futures = [loop.run_in_executor(executor, t) for t in tasks]
+            yield from asyncio.gather(*futures)
+            print('populate_periodic::SLEEP')
+            # TODO: Implement scheduler for this task
+            # try:
+            #     yield from sleep(15)
+            # except Exception as e:
+            #     return print('SLEEP_EXCEPTION', e)
+
+            # yield from periodic(loop, sleep)
+
+    yield from periodic(_loop)
 
 
-loop.run_until_complete(populate_periodic())
+def async_queue():
 
+    # Start a new event loop for async requests
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    # Set and start async work thread
+    work = threading.Thread(target=lambda: loop.run_until_complete(populate_periodic(loop)))
+    work.start()
+
+
+async_queue()
 print('DB_INIT::finished')
-# print('sleeping...')
-# sleep(5)
-# loop.run_until_complete(populate_periodic())
-#
-# populate_channels()
-# populate_users()
-
