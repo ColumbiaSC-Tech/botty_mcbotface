8167370e257ea73986a7ddfdb316bd25db96a891
Danny <danny@nulleffort.com>
2017-11-24 11:28:33 -0500

    refactoring task queue to master daemon, slave blocking/events

--- botty_mcbotface/botty/db/routines/__init__.py
+++ botty_mcbotface/botty/db/routines/__init__.py
@@ -4,10 +4,8 @@ import re
 import asyncio
 import threading
 from apscheduler.schedulers.background import BackgroundScheduler
+from botty_mcbotface.utils.tools import WorkThread
 from importlib import import_module
-
-logger = logging.getLogger(__name__)
-logger.info('INFO WORKING')
 
 # Main scheduler instantiation
 scheduler = BackgroundScheduler(daemon=False)
@@ -29,23 +27,38 @@ def get_default_routines():
 
 def spawn_task_thread(routine):
 
-    print('Jobs::', scheduler.get_jobs())
-    print('Threads::', len(threading.enumerate()))
+    # logging.info('Jobs::{}'.format(scheduler.get_jobs()))
+    logging.info('Threads::{}'.format(len(threading.enumerate())))
 
     # Start a new event loop for async requests
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
     # Spawn and start routine work threads
-    work = threading.Thread(target=lambda: loop.run_until_complete(routine(loop)))
-    return work.start()
+    # work = WorkThread(target=lambda: loop.run_until_complete(routine(loop)), daemon=False)
+    # return work.run_interval(10)
+    work = WorkThread(task=lambda: loop.run_until_complete(routine(loop)), interval=5)
+    work.run()
 
 
 def task_queue_scheduler(routines):
-    print('scheduler started', routines)
-    for r in routines:
-        routine, interval = getattr(import_module('.'.join([__name__, r])), r, 'INTERVAL')
-        scheduler.add_job(lambda: spawn_task_thread(routine), 'interval', seconds=interval)
-        spawn_task_thread(routine)
+    logging.info('scheduler started::{}'.format(routines))
 
-    return scheduler.start()
+    # def routine_queue():
+        # queue = []
+        # for r in routines:
+        #     routine = getattr(import_module('.'.join([__name__, r])), r)
+        #     scheduler.add_job(lambda: spawn_task_thread(routine), 'interval', seconds=5)
+            # queue.append(spawn_task_thread(routine))
+        # return queue
+
+    for r in routines:
+        routine = getattr(import_module('.'.join([__name__, r])), r)
+        scheduler.add_job(lambda: spawn_task_thread(routine), 'interval', seconds=5)
+
+    # print('QUEUE::\n', routine_queue())
+    # main_work_thread = WorkThread(daemon=True, interval=5)
+    # main_work_thread.run_queue(routine_queue)
+    main_work_thread = WorkThread(task=scheduler.start, daemon=True, interval=5)
+    # main_work_thread.run_queue(routine_queue)
+    return main_work_thread.run()
--- botty_mcbotface/utils/tools.py
+++ botty_mcbotface/utils/tools.py
@@ -1,9 +1,85 @@
+import logging
 import requests
+import threading
 from random import choice
+from time import sleep
 from bs4 import BeautifulSoup
 
+log = logging.getLogger()
 
-# *** General Methods *** #
+
+# *** Custom Threading Class *** #
+class WorkThread(object):
+    """ Threading example class
+    The run() method will be started and it will run in the background
+    until the application exits.
+    """
+
+    def __init__(self, task, daemon=False, interval=1):
+        """ Constructor
+        :type interval: int
+        :param interval: Check interval, in seconds
+        """
+        self.interval = interval
+        self.task = task
+        self._stop_event = threading.Event()
+        thread = threading.Thread(target=self.run)
+        thread.daemon = daemon
+        thread.start()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+    def run(self):
+        """ Method that runs forever """
+        while not self.stopped():
+            self.task()
+            print('Thread sleeping...')
+            try:
+                sleep(self.interval)
+            except KeyboardInterrupt:
+                print('Thread cancelled')
+                return 0
+
+    def run_queue(self, task_queue):
+        task_queue()
+        while not self.stopped():
+            print('Queue thread sleeping...')
+            try:
+                sleep(self.interval)
+            except KeyboardInterrupt:
+                print('Thread cancelled')
+                return 0
+
+# class WorkThread(threading.Thread):
+#     """Thread class with a stop() method. The thread itself has to check
+#     regularly for the stopped() condition."""
+#
+#     def __init__(self, *args, **kwargs):
+#         super(WorkThread, self).__init__(*args, **kwargs)
+#         # self.interval = interval
+#         self._stop_event = threading.Event()
+#         # log.info('DAEMON::{}'.format(self.isDaemon()))
+#
+#     def stop(self):
+#         self._stop_event.set()
+#
+#     def stopped(self):
+#         return self._stop_event.is_set()
+#
+#     def run_interval(self, interval):
+#         # Todo: Accept an interval
+#         self.start()
+#
+#         while not self.stopped():
+#             logging.info('IN THREAD LOOP')
+#             sleep(interval)
+#
+
+# *** General Helper Functions *** #
 
 def soup(html):
     """
