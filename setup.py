389fda83b227142f959834aa3cfb3c64b4ec6912
Danny <danny@nulleffort.com>
2018-09-30 14:14:11 -0400

    Update setup dependencies

--- setup.py
+++ setup.py
@@ -9,47 +9,60 @@ def readme():
         return f.read()
 
 
-setup(name='botty_mcbotface',
-      version='1.4.0',
-      description='General purpose Slack-bot with personality',
-      long_description=readme(),
-      url='https://github.com/ColumbiaSC-Tech/botty_mcbotface',
-      author='Danny Hinshaw',
-      author_email='danny@nulleffort.com',
-      classifiers=[
-          #   3 - Alpha
-          #   4 - Beta
-          #   5 - Production/Stable
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.7',
-      ],
-      install_requires=[
-          'apscheduler',
-          'arrow',
-          'bs4',
-          'cchardet',
-          'lxml',
-          'nose',
-          'pytz',
-          'requests',
-          'slackbot',
-          'sqlalchemy'
-      ],
-      keywords='slack bot slackbot botty_mcbotface botty gonzobot',
-      license='MIT',
-      packages=['botty_mcbotface',
-                'botty_mcbotface.botty',
-                'botty_mcbotface.botty.db',
-                'botty_mcbotface.botty.db.models',
-                'botty_mcbotface.botty.db.routines',
-                'botty_mcbotface.data',
-                'botty_mcbotface.plugins',
-                'botty_mcbotface.tasq_runner',
-                'botty_mcbotface.utils'],
-      package_data={
-          '': ['*.json', '*.rst', '*.txt'],
-      },
-      python_requires='>=3.7',
-      zip_safe=False)
+setup(
+    name='botty_mcbotface',
+    version='1.4.0',
+    description='General purpose Slack-bot with personality',
+    long_description=readme(),
+    url='https://github.com/ColumbiaSC-Tech/botty_mcbotface',
+    author='Danny Hinshaw',
+    author_email='danny@nulleffort.com',
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'apscheduler',
+        'arrow',
+        'bs4',
+        'certifi',
+        'cchardet',
+        'google-api-python-client',
+        'google-auth',
+        'idna',
+        'lxml',
+        'nose',
+        'python-dateutil',
+        'pytz',
+        'requests',
+        'slackbot',
+        'slacker',
+        'sqlalchemy'
+        'tzlocal',
+        'urllib3',
+        'websocket-client'
+    ],
+    keywords='slack bot slackbot botty_mcbotface botty gonzobot',
+    license='MIT',
+    packages=[
+        'botty_mcbotface',
+        'botty_mcbotface.botty',
+        'botty_mcbotface.botty.db',
+        'botty_mcbotface.botty.db.models',
+        'botty_mcbotface.botty.db.routines',
+        'botty_mcbotface.data',
+        'botty_mcbotface.plugins',
+        'botty_mcbotface.task_runner',
+        'botty_mcbotface.utils'
+    ],
+    package_data={
+        '': ['*.json', '*.rst', '*.txt'],
+    },
+    python_requires='>=3.7',
+    zip_safe=False
+)
