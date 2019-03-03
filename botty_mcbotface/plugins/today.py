4f92d50a66c64e07b6c785364bca6d1abd5da024
Danny <danny@nulleffort.com>
2019-03-03 12:01:14 -0500

    Fix xml parsing

--- botty_mcbotface/plugins/today.py
+++ botty_mcbotface/plugins/today.py
@@ -1,7 +1,9 @@
-from botty_mcbotface import bot_tz
-from botty_mcbotface.utils.tools import get_html
 from datetime import datetime
+
 from slackbot.bot import listen_to, re
+
+from botty_mcbotface import bot_tz
+from botty_mcbotface.utils.tools import get_xml
 
 
 @listen_to('^.today', re.IGNORECASE)
@@ -14,7 +16,7 @@ def today(message):
     _today = datetime.now(bot_tz).strftime("%A, %B %d, %Y.")
     msg = "_{}_\n*Today's Holidays:*\n".format(_today)
 
-    holidays = get_html("https://www.checkiday.com/rss.php?tz=America/New_York").find_all('title')
+    holidays = get_xml("https://www.checkiday.com/rss.php?tz=America/New_York").find_all('title')
 
     for holiday in holidays:
         if 'checkiday.com' in holiday.text.lower():
--- botty_mcbotface/utils/tools.py
+++ botty_mcbotface/utils/tools.py
@@ -1,18 +1,19 @@
-import requests
-from botty_mcbotface import log
-from bs4 import BeautifulSoup
 from random import choice
+
+import requests
+from bs4 import BeautifulSoup
 
 
 # *** General Helper Functions *** #
 
-def soup(html):
+def soup(html: str, doc_type: str = "lxml"):
     """
     Get a BeautifulSoup object from html
     :param html: HTML string
+    :param doc_type: Document type
     :return: BeautifulSoup object.
     """
-    bs = BeautifulSoup(html, 'lxml')
+    bs = BeautifulSoup(html, doc_type)
 
     return bs
 
@@ -29,12 +30,25 @@ def get_html(url):
     return s
 
 
+def get_xml(url):
+    """
+    Return a BeautifulSoup
+    :param url: URL to get XML from
+    :return: XML of requested URL.
+    """
+    res = requests.get(url)
+    s = soup(res.text, "lxml-xml")
+
+    return s
+
+
 def random_response(responses):
     """
     Return a random response given a list of responses
     :param responses: List of response strings
     :return: A random response string.
     """
+
     def test_1d(res):
 
         if isinstance(res, list):
