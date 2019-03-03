f4b4d3f6d684f612e5d0fe97dbb7eee491fdfe7a
Danny <danny@nulleffort.com>
2019-03-03 12:03:45 -0500

    Bump version -> 1.5.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name='botty_mcbotface',
-    version='1.5.2',
+    version='1.5.3',
     description='General purpose Slack-bot with personality',
     long_description=readme(),
     url='https://github.com/ColumbiaSC-Tech/botty_mcbotface',
